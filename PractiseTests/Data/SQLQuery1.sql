5d89de2ef3e0163e3fe27c3030b41a1cd220e4c9
Priya <itsmepriyap@gmail.com>
2023-08-06 22:03:35 +1000

    Identity module is updated

--- PractiseTests/Data/SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
-﻿select * from Certifications
-
-select  certificationTestid,TestPaperName,Active  from TestPaper where active=0 and certificationTestid=2 
-group by certificationTestid,TestPaperName,Active
-  
-exec sp_help TestPaper
-
-update Certifications set Active=1 
-
